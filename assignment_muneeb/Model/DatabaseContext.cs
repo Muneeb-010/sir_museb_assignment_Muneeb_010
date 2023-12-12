3a86a56d69420fa80c69adba983e92233deadf5e
zain gillani <gillanizain90@gmail.com>
2023-12-12 19:07:15 +0500

    fine all

---
+++ assignment_muneeb/Model/DatabaseContext.cs
@@ -0,0 +1,42 @@
+﻿using assignment_muneeb.data;
+using Microsoft.EntityFrameworkCore;
+
+namespace assignement_muneeb.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Faculty> Faculty { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Enrolled> Enrolled { get; set; }
+        public DbSet<Mark> Marks { get; set; }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+            modelBuilder.Entity<Enrolled>()
+                .HasKey(e => new { e.Eid, e.Sid });
+
+            
+            modelBuilder.Entity<Enrolled>()
+                .HasOne(e => e.Class)
+                .WithMany(c => c.Enrolled)
+                .HasForeignKey(e => e.Sid);
+
+            
+            modelBuilder.Entity<Enrolled>()
+                .HasOne(e => e.Student)
+                .WithMany(s => s.Enrolled)
+                .HasForeignKey(e => e.Sid);
+
+
+        }
+
+    }
+}
---
+++ assignment_muneeb/data/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace assignment_muneeb.data
+{
+    public class Student
+    {
+        [Key]
+        public int Sid { get; set; }
+        public string Sname { get; set; }
+        public string Major { get; set; }
+        public int Standing { get; set; }
+        public ICollection<Enrolled> Enrolled { get; set; }
+    }
+}
